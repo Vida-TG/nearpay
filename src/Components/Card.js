654813df6bfe7ace7a8a5a013ff9d76c829516de
ViDA_TG <victorademiju.t@gmail.com>
2023-05-11 14:24:24 +0100

    Gif and more frontend edits

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../assets/card.css';
 import cancel from '../../assets/cancel.png'
+import giphy from './watching-eyes.gif'
 
 
 export default function Card() {
@@ -29,38 +30,49 @@ export default function Card() {
 
     return (
         <>
-        <div className="full-card" style={ popupState ? { opacity : '.1' } : { opacity : '1' }}>
-            <div className='card-header'>
-                <div onClick={turnOn1} className='switch switch1' style={ switch1 ? { borderBottom : '3px solid black' } : {}}>
-                    RECEIVE
+        {
+            (window.accountId==='')?
+                <div className="full-card card-edit">
+                    <img src={giphy} className='giphy'/>
+                    <div className="before-login">Please Log In</div>
                 </div>
-                <div onClick={turnOn2} className='switch switch2' style={ switch2 ? { borderBottom : '3px solid black' } : {}}>
-                    SEND
-                </div>
-            </div>
-            
-            <div  className='card-body'>
-                <div style={ switch1 ? { display : 'block' } : { display : 'none' }}>
-                    <div>Generate and share QR code to receive payment in Near tokens</div>
-                    <div className="qr-code"></div>
-                    <div>
-                        <input className="text-input amount" placeholder="Amount"/>
-                        <input className="text-input item" placeholder="Reason (or item)"/>
-                        <button className="submit-input" onClick={generateCode}>Generate code</button>
+                
+                :
+                <section>
+                    <div className="full-card" style={ popupState ? { opacity : '.1' } : { opacity : '1' }}>
+                        <div className='card-header'>
+                            <div onClick={turnOn1} className='switch switch1' style={ switch1 ? { borderBottom : '3px solid black' } : {}}>
+                                RECEIVE
+                            </div>
+                            <div onClick={turnOn2} className='switch switch2' style={ switch2 ? { borderBottom : '3px solid black' } : {}}>
+                                SEND
+                            </div>
+                        </div>
+                        
+                        <div  className='card-body'>
+                            <div style={ switch1 ? { display : 'block' } : { display : 'none' }}>
+                                <div>Generate and share QR code to receive payment in Near tokens</div>
+                                <div className="qr-code"></div>
+                                <div>
+                                    <input className="text-input amount" placeholder="Amount"/>
+                                    <input className="text-input item" placeholder="Reason (or item)"/>
+                                    <button className="submit-input" onClick={generateCode}>Generate code</button>
+                                </div>
+                            </div>
+                            <div style={ switch2 ? { display : 'block' } : { display : 'none' }}>
+                                Send to bank
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div style={ switch2 ? { display : 'block' } : { display : 'none' }}>
-                    Send to bank
-                </div>
-            </div>
-        </div>
-        <div className='popup'  style={ popupState ? { display : 'block' } : { display : 'none'}}>
-            <div className='cancel-wrap'><img src={cancel} onClick={closePopup} className='cancel-btn' /></div>
-            <div className='popup-body'>
-                <div className='popup-qr-code'></div>
-                <div>Share</div>
-            </div>
-        </div>
+                    <div className='popup'  style={ popupState ? { display : 'block' } : { display : 'none'}}>
+                        <div className='cancel-wrap'><img src={cancel} onClick={closePopup} className='cancel-btn' /></div>
+                        <div className='popup-body'>
+                            <div className='popup-qr-code'></div>
+                            <div>Share</div>
+                        </div>
+                    </div>
+                </section>
+        }
         </>
     )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ import './header.css'
 const Header = () => {
   return (
     <div className='header'>
-        <button onClick={(window.accountId==='')?loginFullAccess:signOutNearWallet} className='login-toggle'>
+        <button onClick={(window.accountId==='')?loginFullAccess:signOutNearWallet} className={(window.accountId==='')?'login-toggle login-white' :'login-toggle'}>
         {(window.accountId==='')?'Login':window.accountId}
         </button>
     </div>
