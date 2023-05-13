d35161400b3e37a5999ea3da81e9f59d3c3ac021
ViDA_TG <victorademiju.t@gmail.com>
2023-05-13 06:20:30 +0100

    Production

--- near-config.js
+++ near-config.js
@@ -4,16 +4,6 @@ export function getConfig(env) {
   switch (env) {
 
     case 'production':
-    case 'mainnet':
-      return {
-        networkId: 'mainnet',
-        nodeUrl: 'https://rpc.mainnet.near.org',
-        contractName: CONTRACT_NAME,
-        walletUrl: 'https://wallet.near.org',
-        helperUrl: 'https://helper.mainnet.near.org',
-        explorerUrl: 'https://explorer.mainnet.near.org',
-      };
-    case 'development':
     case 'testnet':
       return {
         networkId: 'testnet',
